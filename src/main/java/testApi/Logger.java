be1a2df6ee35dfc4ba5c469ed66a0b3d3e6868d9
esadmin <esadmin@DEVELOP>
2017-09-15 17:54:22 +0900

    Now we can get entities for a custom-model.
    Even though wks has deployed custom-model there are still some necessary
    things should be done to get the entity feature's information into this
    class.
    
    Language setting and JSON management.
    
    For the details see the code.

--- src/main/java/testApi/Logger.java
+++ src/main/java/testApi/Logger.java
@@ -1,16 +1,97 @@
 package testApi;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Random;
+
 public class Logger {
 
 	int k;
+	boolean messageSwitch;
+	
+	String identifier;
+	String log;
+	String logDirectory;
+	
 	public Logger() {
 		// TODO Auto-generated constructor stub
 		this.k = 0;
+		this.messageSwitch = true;
+		this.log = "";
+	}
+	
+	public Logger(boolean message) {
+		this.k = 0;
+		this.messageSwitch = message;
+		this.log = "";
+	}
+	
+	public Logger(boolean message, String identifier) {
+		this.k = 0;
+		this.messageSwitch = message;
+		this.log = "";
+		this.identifier =identifier;
+	}
+	
+	/**
+	 * @param dir
+	 * 
+	 * 생성자를 사용해서 동적으로 로그파일을 생성해보려 했으나 NullPointerException을 뱉으며 생성되지 않는다.
+	 */
+	public Logger(String identifier) {
+		this.identifier = identifier;
+//		logFile(log, logDirectory);
 	}
 	
 	public void l(String message) {
+		if(messageSwitch) {
 		System.out.println(k+". log : "+message);
 		k++;
+		}
+		log += k+". "+message+"\n\r";
+	}
+	
+	public void logFile(String log, String resultDirectory) {
+		try {
+			String fileName = generateFileName();
+			BufferedWriter bwr = new BufferedWriter(
+					new OutputStreamWriter(
+							new FileOutputStream(resultDirectory+fileName, true),"utf-8"));
+			bwr.write(log+"\r\n");
+			bwr.flush();
+			bwr.close();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String generateFileName() {
+//		+"\\"+System.getProperties().getProperty("user.name")+"_"++".txt"
+		int min, max, randomNum;
+		String filename = "";
+		String time = "";
+		String username = System.getProperties().getProperty("user.name");
+		long timeStamp = System.currentTimeMillis();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy_MM_dd HH-mm");
+		time = simpleDateFormat.format(timeStamp);
+		Random random = new Random();
+		min = 0;
+		max = 999999999;
+		randomNum = random.nextInt(max - min + 1) + min;
+		filename = "\\"+time+"_"+username+"_"+randomNum+"_"+identifier+".txt";
+		return filename;
 	}
 
 }
--- src/main/java/testApi/TestApi.java
+++ src/main/java/testApi/TestApi.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.json.JSONException;
@@ -25,6 +26,8 @@ import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Co
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EntitiesOptions;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Features;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.KeywordsOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.ListModelsResults;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.RelationsOptions;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.SentimentOptions;
 
 public class TestApi {
@@ -33,29 +36,59 @@ public class TestApi {
 		// TODO Auto-generated constructor stub
 	}
 
-	Logger t = new Logger();
+	String resultDir = "C:\\Users\\Juet\\Desktop\\업무(PC)\\study\\사장님 연구 과제\\SKStudy\\textResult";
+	String logDir = "C:\\Users\\Juet\\Desktop\\업무(PC)\\study\\사장님 연구 과제\\SKStudy\\log";
 
+//	Logger t = new Logger(false, "classMethod-T");
+//	Logger g = new Logger(false, "classMethod-G");
+	
+	Logger t = new Logger(false);
+	Logger g = new Logger(false);
+
+	/**
+	 * @param args
+	 */
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Logger t = new Logger();
+		Logger t = new Logger(true, "insideMain-T");
 		TestApi d = new TestApi();
-//		List<String> contents = d.getFiles("C:\\Users\\Juet\\Desktop\\업무(PC)\\study\\사장님 연구 과제\\SKStudy\\text");
-		List<String> contents = d.getFiles("C:\\Users\\Juet\\Desktop\\업무(PC)\\study\\사장님 연구 과제\\SKStudy\\사장님Sample\\106_테스트 데이터_170914");
 
+		/* 동적으로 코드를 로그 파일을 생성해 보려고 했으나 메인 메서드 내부의 로그를 파일로 옮긴는 것은 성공했지만 메인메서드 밖에 메서드에서 호출한 로그클래스로는 파일은 생성되지만 내용은 찍히지 않는 현상을
+		 * 경험하고 있다. 안드로이드라면 */
+//		d.logStart();
+		// List<String> contents =
+		// d.getFiles("C:\\Users\\Juet\\Desktop\\업무(PC)\\study\\사장님 연구
+		// 과제\\SKStudy\\text");
+		List<String> contents = d
+				.getFiles("C:\\Users\\Juet\\Desktop\\업무(PC)\\study\\사장님 연구 과제\\SKStudy\\사장님Sample\\106_테스트 데이터_170914");
+
+		// bluemixID : sbkyo@naver.com
+		// NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(
+		// NaturalLanguageUnderstanding.VERSION_DATE_2017_02_27,
+		// "4dd8d745-fd2c-4436-988c-9adf62df123f",
+		// "5MV2ODutICGc");
+
+		// bluemixID : sbkyo88@gmail.com
 		NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(
-				NaturalLanguageUnderstanding.VERSION_DATE_2017_02_27, "4dd8d745-fd2c-4436-988c-9adf62df123f",
-				"5MV2ODutICGc");
+				NaturalLanguageUnderstanding.VERSION_DATE_2017_02_27, "cf0a651e-5371-458c-acbc-837bdb00f58f",
+				"0MsDxmo4sgV5");
 
-		// ListModelsResults models = service.getModels().execute();
-		// t.l("models : "+models);
-
+		ListModelsResults models = service.getModels().execute();
+		t.l("models : " + models);
+		System.out.println(models);
+		String modelId = "" + d.getWKSModels(models).get("model_0").get("modelId");
+		t.l("modelId : " + modelId);
 		// String text = "IBM is an American multinational technology company
 		// headquartered in Armonk, New York, United States, with operations in over 170
 		// countries.";
 
-		EntitiesOptions entitiesOptions = new EntitiesOptions.Builder().emotion(true).sentiment(true).limit(2).build();
+		// EntitiesOptions entitiesOptions = new
+		// EntitiesOptions.Builder().model(modelId).emotion(true).sentiment(true).limit(2).build();
+		EntitiesOptions entitiesOptions = new EntitiesOptions.Builder().model(modelId).build();
+
+		RelationsOptions relationsOptions = new RelationsOptions.Builder().model(modelId).build();
 
 		KeywordsOptions keywordsOptions = new KeywordsOptions.Builder().emotion(true).sentiment(true).limit(2).build();
 
@@ -63,25 +96,47 @@ public class TestApi {
 
 		CategoriesOptions categoriesOptions = new CategoriesOptions();
 
-		ConceptsOptions conceptsOptions = new ConceptsOptions.Builder().limit(50).build();
+		ConceptsOptions conceptsOptions = new ConceptsOptions.Builder().limit(8).build();
 
-		Features features = new Features.Builder().entities(entitiesOptions).keywords(keywordsOptions)
-				.categories(categoriesOptions).concepts(conceptsOptions).sentiment(sentimentOptions).build();
+		Features features = new Features.Builder().entities(entitiesOptions).relations(relationsOptions)
+				.keywords(keywordsOptions).categories(categoriesOptions).concepts(conceptsOptions)
+				.sentiment(sentimentOptions).build();
+		Features entityFeatures = new Features.Builder().entities(entitiesOptions).build();
 
 		AnalyzeOptions parameters = null;
+		AnalyzeOptions entityParameters = null;
+
 		AnalysisResults response = null;
+		AnalysisResults entityResponse = null;
 
 		JSONArray jArray = new JSONArray();
-		for (int q = 0; q < contents.size(); q++) {
-			parameters = new AnalyzeOptions.Builder().text(contents.get(q)).features(features).returnAnalyzedText(true)
-					.build();
-			response = service.analyze(parameters).execute();
-			// t.l("contents : "+contents.get(q)+"\nresponse : "+response);
-			JSONObject jObj = new JSONObject(response);
-			jArray.add(jObj);
+		try {
+			for (int q = 0; q < contents.size(); q++) {
+				parameters = new AnalyzeOptions.Builder().text(contents.get(q)).features(features)
+						.returnAnalyzedText(true).build();
+				entityParameters = new AnalyzeOptions.Builder().language("en").text(contents.get(q))
+						.features(entityFeatures).returnAnalyzedText(true).build();
+
+				response = service.analyze(parameters).execute();
+				entityResponse = service.analyze(entityParameters).execute();
+//				t.l("entityResponse : " + entityResponse);
+				// t.l("contents : "+contents.get(q)+"\nresponse : "+response);
+				JSONObject entityObject = new JSONObject(entityResponse);
+				org.json.JSONArray entityJArray = entityObject.getJSONArray("entities");
+				JSONObject jObj = new JSONObject(response);
+				jObj.put("entities", entityJArray);
+				jArray.add(jObj);
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
 		}
-		t.l("jArray raw contents : " + jArray);
-		d.jsonToText(jArray);
+		t.l("{\"jArray raw contents\":" + jArray + "}");
+		List<String> contentsList = d.jsonToText(jArray);
+		t.l("listSize: " + contentsList.size());
+		for (String str : contentsList) {
+			t.l(str);
+		}
+		t.logFile(t.log, d.logDir);
 	}
 
 	public List<String> writeFiles(String dir, List<String> contents) {
@@ -113,7 +168,7 @@ public class TestApi {
 	 *            다만 하드코딩을 벗어나서 코딩을 해보자는 생각에 이렇게 복잡한 중첩 반복문을 사용하게 되었다. 그다지 효율적인 코드라고
 	 *            할 수 없다.
 	 */
-	public void jsonToText(JSONArray jArray) {
+	public List<String> jsonToText(JSONArray jArray) {
 		// t.l("jsonArray in method : \n"+jArray);
 
 		int listCount = 1;
@@ -130,7 +185,11 @@ public class TestApi {
 				for (String jNames : jsonNames) {
 					t.l("\n\r" + f + ". " + "jsonNames in method: " + jNames + "\n\r");
 					if (jNames != null && (jNames.equals("language") || jNames.equalsIgnoreCase("analyzedText"))) {
-						contents.add(jNames+" - "+jsonObject.getString(jNames));
+						if (!jNames.equals("analyzedText") && jsonNames.contains("entities")) {
+							contents.add(jNames + " - " + jsonObject.getString(jNames)+" [and en(Entity)]");
+						} else {
+							contents.add(jNames + " - " + jsonObject.getString(jNames));
+						}
 						continue;
 					}
 					try {
@@ -151,7 +210,11 @@ public class TestApi {
 							listCount++;
 						}
 						String editRow = row.substring(0, row.length() - 2);
-						contents.add(jNames + " - " + editRow);
+						if (jNames.equals("usage") && jsonNames.contains("entities")) {
+							contents.add(jNames + " - " + editRow + " [and Entity(en)]");
+						} else {
+							contents.add(jNames + " - " + editRow);
+						}
 
 					} catch (JSONException e) {
 						e.printStackTrace();
@@ -167,7 +230,7 @@ public class TestApi {
 								t.l(z + ". aObjNames : " + aObjNames[z]);
 								String value = "" + aObj.get(JSONObject.getNames(aObj)[z]);
 								t.l(f + "-2. aObj : " + aObj.get(JSONObject.getNames(aObj)[z]));
-								t.l(f + "-2" + listCount + ". key : " + key + ", value : " + value);
+								t.l(f + "-2" + "-" + listCount + ". key : " + key + ", value : " + value);
 								// contents.add(key+" : "+value);
 								row += key + " : " + value + ", ";
 							}
@@ -176,14 +239,16 @@ public class TestApi {
 						}
 					}
 				}
-			} 
+			}
 			for (String string : contents) {
-				t.l("contents Check : " + string);
+				// t.l("contents Check : " + string);
+				// writeFiles(resultDir, string);
 			}
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return contents;
 	}
 
 	public List<String> getFiles(String dirPath) {
@@ -234,6 +299,39 @@ public class TestApi {
 			e.printStackTrace();
 		}
 		return fileContentList;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public HashMap<String, HashMap> getWKSModels(ListModelsResults models) {
+		HashMap<String, HashMap> jNameValueMap = new HashMap<String, HashMap>();
+		HashMap<String, String> numJNameValueMap = new HashMap<String, String>();
+		try {
+			JSONObject jsonObject = new JSONObject(models);
+			org.json.JSONArray jsonArray = jsonObject.getJSONArray("models");
+			for (int j = 0; j < jsonArray.length(); j++) {
+				JSONObject jObject = jsonArray.getJSONObject(j);
+				g.l("jObject : " + jObject);
+				g.l("jObject model id : " + jObject.get("modelId"));
+				String[] jNames = JSONObject.getNames(jObject);
+				String[] jNameValues = new String[jNames.length];
+				for (int h = 0; h < jNames.length; h++) {
+					jNameValues[h] = "" + jObject.get(jNames[h]);
+					numJNameValueMap.put(jNames[h], jNameValues[h]);
+					g.l(h + ". jNames : " + jNames[h] + ", jValues : " + jNameValues[h]);
+				}
+				jNameValueMap.put("model_" + j, numJNameValueMap);
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return jNameValueMap;
+	}
+
+	public void logStart() {
+		t.logFile(t.log, logDir);
+		g.logFile(g.log, logDir);
 	}
 
 }
